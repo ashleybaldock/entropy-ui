6615ba76090104d0c05921592d0232418bb2646c
Ashley Baldock <ashley@morepossibility.com>
2022-08-07 03:14:51 +0100

    Upgrade deps

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
   stories: ['../src/**/*.stories.js'],
   addons: [
+    '@storybook/preset-create-react-app',
     '@storybook/addon-actions',
     '@storybook/addon-controls',
     '@storybook/addon-links',
@@ -11,31 +12,14 @@ module.exports = {
     // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
     // You can change the configuration based on that.
     // 'PRODUCTION' is used when building the static version of storybook.
-
     // Make whatever fine-grained changes you need
     config.resolve.modules.push(path.join(__dirname, '../src'));
 
-    config.module.rules.find(
-        rule => rule.test.toString() === '/\\.css$/',
-      ).exclude = /\.module\.css$/;
-
-    config.module.rules.push(
-      {
-        test: /\.module\.css$/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true
-            },
-          },
-        ],
-        include: /\.module\.css$/,
-      }
-    );
-
     // Return the altered config
+
     return config;
+  },
+  core: {
+    builder: 'webpack5',
   },
 };
